b43680ef99f0091f745053a439a34a365475d5ea
dragrom <dragrom@gmail.com>
2017-10-17 11:06:21 +0300

    Update init.pp
    Arranged

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,14 +11,12 @@
 # Sample Usage:
 #
 class nettime (
-  $nettime_install_location         = $::nettime::params::install_location,
-  $nettime_download_url        = $::nettime::params::download_url,
-  $enable_autouninstaller         = $::nettime::params::enable_autouninstaller,
-  $log_output                     = false
+  $nettime_install_location = $::nettime::params::install_location,
+  $nettime_download_url     = $::nettime::params::download_url,
+  $enable_autouninstaller   = $::nettime::params::enable_autouninstaller,
+  $log_output               = false
 ) inherits ::nettime::params {
   
-       class { '::nettime::install': } #->
-#      class { '::nettime::config': }
-  
+       class { '::nettime::install': }
 }
 
