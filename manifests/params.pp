9900cb98b21077a543220e5976aff51077c43ed3
dragrom <dragrom@gmail.com>
2017-10-17 11:08:06 +0300

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,5 @@
 class nettime::params {
-  $install_location         = 'C:/NetTime' # default is C:\ProgramData\NetTime
-  $download_url             = 'http://www.timesynctool.com/NetTimeSetup-314.exe'
-  $enable_autouninstaller   = true
+    $install_location       = 'C:/NetTime' # default is C:\ProgramData\NetTime
+    $download_url           = 'http://www.timesynctool.com/NetTimeSetup-314.exe'
+    $enable_autouninstaller = true
 }
